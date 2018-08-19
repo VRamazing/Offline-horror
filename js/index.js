19175fa19828cd3f7fea534ed5b4f1c39b239f12
techieVignesh <r.vignesh149@gmail.com>
2018-08-19 21:12:23 +0530

    Working on intial story

--- js/index.js
+++ js/index.js
@@ -3,27 +3,58 @@ var storyList = [
   {text: "It's just another morning. You returned from school which was such a bore. You decided to spend your time playing Xbox with your gaming buddies. You log into your system and try to connect. It's taking too long to load.",
   optionsIndex: 0
   },
-  {text: "Lorem Ipsum",
-  optionsIndex: 1
-  },
-  {text: "Dollar Imet",
+  {text: "You tapped on the box. Nothing happened. You saw a spider running frantically",
   optionsIndex: 0
   },
-  {text: "Fucking shit",
+  {text: "You prayed for faster load. Still nothing",
+  optionsIndex: 0
+  },
+  {text: "Checked for internet connection. There is no network. That's odd. This has never happened before.",
   optionsIndex: 1
-  }         
+  },
+  {text: "You tried calling your network provider but there is no cellular network as well",
+  optionsIndex: 1
+  },
+  {text: "You give a call to mom but there is no response.",
+  optionsIndex: 2
+  },
+  {text: "You storm back into your room. You see a dirty room with nowhere to lay down.",
+  optionsIndex: 3
+  }     
+     
+
 ];
 
 var optionsList = [
    [
      {text: 'Check internet connection',
-      resultStoryIndex: 1
+      resultStoryIndex: 3
      },
      {text: 'Tap on the Xbox',
-      resultStoryIndex: 2
+      resultStoryIndex: 1
      },
      {text: 'Pray for faster load',
-      resultStoryIndex: 3
+      resultStoryIndex: 2
+     }
+   ],
+   [
+     {text: 'Call the network provider',
+      resultStoryIndex: 4
+     },
+     {text: 'Ask mom if she payed bill',
+      resultStoryIndex: 5
+     },
+     {text: 'Storm angrily back to room cause your mood is destroyed.',
+      resultStoryIndex: 6
+     }
+   ],
+
+   [
+     {text: 'Call the network provider',
+      resultStoryIndex: 4
+     },
+     {text: 'Storm angrily back to room cause your mood is destroyed.',
+      resultStoryIndex: 6
      }
    ],
 
